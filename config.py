be109c18277951b7e93463cc28a4a9cc00edf5e0
Ricardo Conceição <75455972+runniOp@users.noreply.github.com>
2025-01-09 16:54:21 +0000

    Alteracoes na config

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SECRET_KEY = os.getenv("SECRET_KEY=mysecretkey")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI=sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
